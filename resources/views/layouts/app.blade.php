3dec36e46f9eb85831b35c4a184890446ee9d7a1
JulieStalker <stalker.julie@gmail.com>
2020-07-18 14:34:30 +1200

    users menu shows if admin user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,9 +49,15 @@
                                 </li>
                             @endif
                         @else
+                            @if (Auth::user()->role == "admin")
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('users.index') }}">Users</a>
+                                </li>
+                                
+                            @endif
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::user()->name }}<span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
